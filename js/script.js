c812d553c25d904f6baa4148eb3bfab24fa45fa5
watarmeli <watar@ukr.net>
2021-02-03 21:32:31 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const pizzaPalace = {
+  pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+  // Пиши код ниже этой строки
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+    }
+
+    return `Заказ принят, готовим пиццу «${pizzaName}».`;
+  },
+  // Пиши код выше этой строки
+};
