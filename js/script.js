055ebec9c4f3239fb904008551e6de5e128660a7
watarmeli <watar@ukr.net>
2021-02-07 14:11:31 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const pizzaPalace = {
+  company: 'Pizza Palace',
+};
+
+const burgerShack = {
+  company: 'Burger Shack',
+};
+
+function composeMessage(customerName) {
+  return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+}
+// Пиши код ниже этой строки
+
+const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+const burgerShackComposer = composeMessage.bind(burgerShack);
+const burgerShackMessage = burgerShackComposer('Поли');
